603fbe86f2dbf4b2b696ad0dee1d5bef75c101b9
blaineconnor <112892044+blaineconnor@users.noreply.github.com>
2024-01-31 17:59:50 +0300

    Final

---
+++ FilmlerApp/Filmler.swift
@@ -0,0 +1,8 @@
+//
+//  Filmler.swift
+//  FilmlerApp
+//
+//  Created by Fatih Emre Sarman on 31.01.2024.
+//
+
+import Foundation
---
+++ FilmlerApp/FilmlerHucre.swift
@@ -0,0 +1,12 @@
+//
+//  FilmlerHucre.swift
+//  FilmlerApp
+//
+//  Created by Fatih Emre Sarman on 31.01.2024.
+//
+
+import UIKit
+
+class FilmlerHucre: UICollectionViewCell {
+    
+}
