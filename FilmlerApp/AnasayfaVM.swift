1e8ff74f1862a25fd3b1de003422eef58f4c5788
blaineconnor <112892044+blaineconnor@users.noreply.github.com>
2024-02-05 09:24:17 +0300

    Veritabanı bağlandı.

--- FilmlerApp/AnasayfaVM.swift
+++ FilmlerApp/AnasayfaVM.swift
@@ -13,11 +13,26 @@ class AnasayfaVM{
     var filmlerListesi = BehaviorSubject<[Filmler]>(value: [Filmler]())
     
     init(){
+        veritabaniKopyala()
         filmlerListesi = frepo.filmlerListesi
         filmleriYukle()
     }
     
     func filmleriYukle(){
         frepo.filmleriYukle()
+    }
+    
+    func veritabaniKopyala(){
+        let bundleYolu = Bundle.main.path(forResource: "filmler", ofType: ".sqlite")
+        let hedefYol = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let kopyalanacakYer = URL(fileURLWithPath: hedefYol).appendingPathComponent("filmler.sqlite")
+        let fileManager = FileManager.default
+        if fileManager.fileExists(atPath: kopyalanacakYer.path){
+            print("Veritabanı zaten var.")
+        }else{
+            do{
+                try fileManager.copyItem(atPath: bundleYolu!, toPath: kopyalanacakYer.path)
+            }catch{}
+        }
     }
 }
